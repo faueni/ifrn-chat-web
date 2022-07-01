b63a1d8738b9279d417bc1639e65825ce0220912
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-06-30 23:15:40 -0300

    modificada pagina de login para uso de estado de componente

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {AppContainer, AppButton, AppInput} from '../src/components';
 
 const Login = props => {
     const router = useRouter();
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeName = (event) => {
+        setName(event.target.value);
+    }
 
     return (
         <div>
@@ -15,9 +21,19 @@ const Login = props => {
             <main>
                 <AppContainer>
 
-                    <AppInput title="apelido ou email do usuário" label="Usuário" type="text" />
+                    <AppInput 
+                        title="apelido ou email do usuário" 
+                        label="Usuário" 
+                        value={name}
+                        onChange={handleChangeName}
+                    />
 
-                    <AppInput title="senha do usuário" label="Senha" type="password" />
+                    <AppInput 
+                        label="Senha" 
+                        type="password" 
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
 
                     <AppButton 
                         onClick={event => router.back()}
@@ -27,6 +43,8 @@ const Login = props => {
                     <AppButton 
                         title="Clique aqui para entrar no chat"
                         label="Entrar no chat"
+                        variant='contained'
+                        disabled={!(!!name && !!password)}
                     />
 
                 </AppContainer>
