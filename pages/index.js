48971e3aabec77a29d5345c0fa34ac5be8f82c92
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-07-06 17:55:10 -0300

    criado estado da pagina home

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 const AppLoginLogoutButton = (props) => {
-  if (props.login) {
-     return <AppButton color="inherit" label="Login" href="/login" />;
+  if (!props.login) {
+    return <AppButton color="inherit" label="Login" href="/login" />;
   } else {
      return (
         <AppButton
@@ -23,42 +23,47 @@ const AppLoginLogoutButton = (props) => {
 };
 
 const AppNavBar = (props) => {
-   return (
-      <AppBar position="static">
-         <Toolbar>
-            <IconButton
-               size="large"
-               edge="start"
-               color="inherit"
-               aria-label="menu"
-               sx={{ mr: 2 }}
-            >
-               <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-               {props.title}
-            </Typography>
-            <AppLoginLogoutButton login/>        
-				 </Toolbar>
-      </AppBar>
-   );
+  return (
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+          >
+              <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {props.title}
+          </Typography>
+          <AppLoginLogoutButton login={props.userLogged} />        
+        </Toolbar>
+    </AppBar>
+  );
 };
 
 export default function Home() {
-   return (
-      <div>
-         <Head>
-            <title>Chat de Infoweb</title>
-            <meta name="description" content="Chat do curso de Infoweb" />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
 
-         <main>
-            <AppNavBar
-               title="Chat de Infoweb"
-            />
-            <h1>Landing page do chat de Infoweb</h1>
-         </main>
-      </div>
-   );
+  const [sessionId, setSessionId] = useState("");
+  const isUserLogged = () => (sessionId ? true : false);
+
+  return (
+    <div>
+        <Head>
+          <title>Chat de Infoweb</title>
+          <meta name="description" content="Chat do curso de Infoweb" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <AppNavBar
+              title="Chat de Infoweb"
+              userLogged={isUserLogged}
+          />
+          <h1>Landing page do chat de Infoweb</h1>
+        </main>
+    </div>
+  );
 }
